3331c4eccbb900fc6eb17d5ce1701edd35bd5a86
Aria van Pelt <ikori@gmx.org>
2018-09-15 11:13:22 +0200

    Scripts for yarn tasks updated

--- scripts/node/yarn-start-test.cmd
+++ scripts/node/yarn-start-test.cmd
@@ -1,14 +1,18 @@
 @echo off
 
-setlocal
+setlocal EnableDelayedExpansion
 call "%~dp0config.cmd"
 
 set "mypackage=%~n0"
 set "mypackage=%mypackage:*-=%
 set "mypackage=%mypackage:*-=%
 
+set "mycommand=%~n0"
+set "mycommand=!mycommand:-%mypackage%=!"
+set "mycommand=%mycommand:-= %"
+
 pushd "%myroot%\prj\node\%mypackage%"
-call yarn start
+call %mycommand%
 popd
 endlocal
 
